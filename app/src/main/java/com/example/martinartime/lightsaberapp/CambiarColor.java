f1bca888aa674937cd975fb123eb33f0efdbc26f
CrimsonBraixen <martin.artime@hotmail.com>
2017-09-12 11:55:09 -0300

    Agregado de conexion bluetooth

--- app/src/main/java/com/example/martinartime/lightsaberapp/CambiarColor.java
+++ app/src/main/java/com/example/martinartime/lightsaberapp/CambiarColor.java
@@ -1,5 +1,6 @@
 package com.example.martinartime.lightsaberapp;
 
+import android.bluetooth.BluetoothSocket;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.os.Handler;
@@ -10,6 +11,8 @@ import android.widget.Toast;
 
 import com.madrapps.pikolo.HSLColorPicker;
 import com.madrapps.pikolo.listeners.SimpleColorSelectionListener;
+
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,6 +26,10 @@ public class CambiarColor extends AppCompatActivity {
 
     @BindView (R.id.circuloCentral) ImageView circuloCentral;
 
+    /**
+     * Obtener el input de color elegido por el usuario y enviarselo al Arduino por bluetooth
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,8 +41,16 @@ public class CambiarColor extends AppCompatActivity {
         colorPicker.setColorSelectionListener(new SimpleColorSelectionListener() {
             @Override
             public void onColorSelected(int color) {
-                // Do whatever you want with the color
                 circuloCentral.getBackground().setColorFilter(color, PorterDuff.Mode.MULTIPLY);
+
+                BluetoothSocket socket = Bluetooth.getMsocket();
+                if(socket!=null){
+                    try {
+                        socket.getOutputStream().write(color);
+                    } catch (IOException e) {
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
     }
--- app/src/main/java/com/example/martinartime/lightsaberapp/MainActivity.java
+++ app/src/main/java/com/example/martinartime/lightsaberapp/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.martinartime.lightsaberapp;
 
+import android.app.Dialog;
+import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.SystemClock;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -22,15 +30,52 @@ public class MainActivity extends AppCompatActivity {
     @BindView (R.id.colores) ImageView colores;
     @BindView (R.id.luz) ImageView luz;
     @BindView (R.id.vibrar) ImageView vibrar;
+    Bluetooth bluetooth;
 
+    /**
+     * Se hace el binding de las views
+     * Se obtienen los equipos bluetooth apareados
+     * Muestra la conexion actual
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
+        bluetooth = new Bluetooth(getApplicationContext());
+
+        ArrayList<BluetoothDevice> equipos = bluetooth.establecerConexionBluetooth(this);
+
+        if(equipos!=null){
+            Dialog dialog = crearDialogo(equipos.get(0).getName());
+            dialog.show();
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "No se encontraron Equipos bluetooth", Toast.LENGTH_LONG).show();
+        }
     }
 
+    /**
+     * Crea el mensaje con el dispositivo apareado que llega por parametro
+     * @param nombre
+     * @return Dialog
+     */
+    public Dialog crearDialogo(String nombre) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Dispositivos apareados")
+                .setMessage(nombre);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {}
+        });
+        return builder.create();
+    }
+
+    /**
+     * Iniciar actividad CambiarColor
+     */
     @OnClick(R.id.cardColores)
     public void cambiarColor(){
         Intent intent = new Intent(getApplicationContext(), CambiarColor.class);
@@ -43,12 +88,18 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    /**
+     * Iniciar actividad AjustarLuz
+     */
     @OnClick(R.id.cardLuz)
     public void ajustarLuz(){
         Intent intent = new Intent(getApplicationContext(), AjustarLuz.class);
         startActivity(intent);
     }
 
+    /**
+     * Iniciar actividad HacerVibrar
+     */
     @OnClick(R.id.cardVibrar)
     public void hacerVibrar(){
         Intent intent = new Intent(getApplicationContext(), HacerVibrar.class);
